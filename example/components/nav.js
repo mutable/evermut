1227eddef76c23ce9306c1a20c4888156a49f93a
Lusine <lusine.terpetrosyan@gmail.com>
2019-08-27 14:33:26 +0400

    sticky menu

--- example/components/nav.js
+++ example/components/nav.js
@@ -1,56 +1,57 @@
 import React from 'react'
 import Link from 'next/link'
+import { StickyMenu } from 'evermut'
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+const primaryMenu = [
+  {
+    // logo: <Logo src={logo} />,
+    props: {
+      href: '/',
+      target: '_blank',
+      paddingTop: 10
+    }
+  },
+  {
+    name: 'Stacks',
+    icon: 'stacked-chart',
+    props: {
+      href: '/',
+      target: '_blank',
+      paddingTop: 10
+    }
+  }
+];
+
+const secondaryMenu = [
+  {
+    name: 'Account Settings',
+    icon: 'settings',
+    props: {
+      href: '/',
+      target: '_blank',
+      paddingTop: 10
+    }
+  },
+  {
+    name: 'Log-out',
+    icon: 'log-out',
+    props: {
+      href: '#',
+      paddingTop: 10,
+      onSelect: () => onSelect('Logout')
+    }
+  },
+]
+
+const onSelect = (item) => {
+  console.log('item', item)
+}
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
+  <StickyMenu
+    primaryMenu={primaryMenu}
+    secondaryMenu={secondaryMenu}
+  />
 )
 
 export default Nav
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,43 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { Pane } from 'evergreen-ui'
 import Nav from '../components/nav'
 
 const Home = () => (
-  <div>
+  <Pane>
     <Head>
       <title>Home</title>
     </Head>
 
     <Nav />
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+    <Pane>
+      Some components
+    </Pane>
 
     <style jsx>{`
       .hero {
@@ -85,7 +62,7 @@ const Home = () => (
         color: #333;
       }
     `}</style>
-  </div>
+  </Pane>
 )
 
 export default Home
