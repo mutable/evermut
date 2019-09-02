5b188b029e0eb9ed10283c577155ec00e09b24d2
Lusine <lusine.terpetrosyan@gmail.com>
2019-09-02 16:36:48 +0400

    add webpack for dev

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    libraryTarget: 'commonjs2',
+    filename: 'evermut.js',
+    chunkFilename: '[name].[chunkhash:8].evermut.js',
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: {
+              compact: true,
+            }
+          },
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'img/[name].[hash:8].[ext]',
+            },
+          },
+          {
+            test: /\.svg$/,
+            loader: 'svg-inline-loader'
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader
+              },
+              'css-loader'
+            ]
+          },
+          {
+            loader: 'file-loader',
+            // Exclude `js` files to keep "css" loader working as it injects
+            // it's runtime that would otherwise processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            options: {
+              name: 'assets/[name].[hash:8].[ext]',
+            },
+          },
+          // Make sure to add the new loader(s) before the "file" loader.
+        ]
+      },
+    ]
+  },
+  externals: {
+    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
+};
+
+module.exports = config;
