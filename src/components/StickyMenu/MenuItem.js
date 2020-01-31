22c4aea2f39410fb8c799082670a2891022b54fa
anna_narimanyan@edu.aua.am <anna_narimanyan@edu.aua.am>
2020-01-31 16:51:06 +0400

    added new style

--- src/components/StickyMenu/MenuItem.js
+++ src/components/StickyMenu/MenuItem.js
@@ -16,7 +16,17 @@ class MutMenuItem extends React.Component {
     const prop = props && Object.keys(props) && Object.keys(props).length ? props : null;
     const pr = icon ? (<Icon size={24} color="#ffffff" icon={icon} />) : null;
     let backgroundColor = 'transparent';
+    let shadow = false;
+
     if(hovered || active) backgroundColor = selectedColor;
+    
+    if(active) shadow = true;
+
+    const style = { backgroundColor };
+
+    if (shadow) {
+      style.boxShadow = "inset 0px 0px 2px 0px rgba(0,0,0,0.15)";
+    }
 
     return (
       <Menu.Item
@@ -29,7 +39,7 @@ class MutMenuItem extends React.Component {
         marginBottom='2px'
         textDecoration='none'
         textAlign='center'
-        style={{ backgroundColor }}
+        style={style}
         onMouseEnter={() => this.setState({ hovered: true })}
         onMouseLeave={() => this.setState({ hovered: false })}
         onSelect={() => changeSelectedColor(children)}
@@ -40,6 +50,7 @@ class MutMenuItem extends React.Component {
           color="#ffffff"
           flexDirection='column'
           display='flex'
+          lineHeight={1.2}
           alignItems='center'
         >
           {pr}{children || null}
