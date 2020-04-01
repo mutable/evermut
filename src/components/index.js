47c73373c71fed08e59052e759d8c04aa27a253c
Anna <anna_narimanyan@edu.aua.am>
2020-04-01 21:11:17 +0400

    back button added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as BackButton } from './BackButton';
 export { default as BreadCrumbs } from './BreadCrumbs';
 
 export { default as CircularProgressBar } from './CircularProgressBar';
