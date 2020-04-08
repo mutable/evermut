9566ed83d0222ef2bb9a007e39c5b5d8b7161582
Anna <anna_narimanyan@edu.aua.am>
2020-04-08 18:22:47 +0400

    horizontal menu added

--- src/components/HorizontalMenu/HorizontalMenu.js
+++ src/components/HorizontalMenu/HorizontalMenu.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Pane, Text, SideSheet, Icon, IconButton } from 'evergreen-ui';
-// import { Logo } from 'evermut';
-// import logo from "../../logo-light.svg";
-// import logoDark from "../../logo-dark.svg";
 
 const PADDING = 15;
 const MARGIN = 15;
 const FONT_SIZE = 16;
 const MENU_HEIGHT = 63;
-const LOGO_HEIGHT = 40;
 const COLORS = {
   DEFAULT: 'white',
   SELECTED: '#f5f6f7',
@@ -25,91 +21,51 @@ class HorizontalMenu extends React.Component {
     }
   }
 
-  _getBackground = item => {
-    const { path } = this.props;
-
-    let background = COLORS.DEFAULT;
-    if (path.includes(item)) background = COLORS.SELECTED;
-
-    return background;
-  }
-
   getBurgerMenu() {
-    const { path } = this.props;
+    const { primaryMenu, secondaryMenu, selectedColor } = this.props;
 
     return (
       <Pane margin={MARGIN*2}>
-        <Text
-          cursor="pointer"
-          fontSize={FONT_SIZE}
-          display='flex'
-          alignItems='center'
-          background={this._getBackground("stacks")}
-          paddingBottom={PADDING}
-          paddingTop={PADDING}
-          paddingLeft={PADDING}
-          onClick={() => this.handleChange('/stacks')}
-          borderBottom='default'
-        >
-          <Icon icon="stacked-chart" marginRight={MARGIN} size={16} /> Stacks
-        </Text>
-        <Text
-          cursor="pointer"
-          fontSize={FONT_SIZE}
-          display='flex'
-          alignItems='center'
-          background={this._getBackground("appDocumentation")}
-          paddingBottom={PADDING}
-          paddingTop={PADDING}
-          paddingLeft={PADDING}
-          onClick={() => this.handleChange('/appDocumentation')}
-          borderBottom='default'
-        >
-          <Icon icon="document" marginRight={MARGIN} size={16} /> Local App Docs
-        </Text>
-        <Text
-          cursor="pointer"
-          fontSize={FONT_SIZE}
-          display='flex'
-          alignItems='center'
-          background={this._getBackground("settings")}
-          paddingBottom={MARGIN}
-          paddingTop={MARGIN}
-          paddingLeft={MARGIN}
-          onClick={() => this.handleChange('/settings')}
-        >
-          <Icon icon="settings" marginRight={MARGIN} size={16} /> Settings
-        </Text>
-        <Text
-          cursor="pointer"
-          fontSize={FONT_SIZE}
-          display='flex'
-          background={this._getBackground("account")}
-          alignItems='center'
-          paddingBottom={MARGIN}
-          paddingTop={MARGIN}
-          paddingLeft={MARGIN}
-          onClick={() => this.handleChange('/account')}
-        >
-          <Icon icon="people" marginRight={MARGIN} size={16} /> Account
-        </Text>
-        <Text
-          cursor="pointer"
-          fontSize={FONT_SIZE}
-          display='flex'
-          alignItems='center'
-          paddingBottom={MARGIN}
-          paddingTop={MARGIN}
-          paddingLeft={MARGIN}
-          onClick={() => this.handleChange('/logout')}
-        >
-          <Icon icon="log-out" marginRight={MARGIN} size={16} /> Log Out
-        </Text>
+        {primaryMenu && primaryMenu.map((item, index) => {
+          const { name, props, icon, active } = item;
+          return (
+            <Text
+              cursor="pointer"
+              fontSize={FONT_SIZE}
+              display='flex'
+              alignItems='center'
+              background={active && selectedColor}
+              paddingBottom={PADDING}
+              paddingTop={PADDING}
+              paddingLeft={PADDING}
+              {...props}
+            >
+              <Icon icon={icon} marginRight={MARGIN} size={16} /> {name}
+            </Text>
+          )
+        })}
+        <Pane borderBottom='default'></Pane>
+        {secondaryMenu && secondaryMenu.map((item, index) => {
+          const { name, props, icon, active } = item;
+          return (
+            <Text
+              cursor="pointer"
+              fontSize={FONT_SIZE}
+              display='flex'
+              alignItems='center'
+              background={active && selectedColor}
+              paddingBottom={PADDING}
+              paddingTop={PADDING}
+              paddingLeft={PADDING}
+              {...props}
+            >
+              <Icon icon={icon} marginRight={MARGIN} size={16} /> {name}
+            </Text>
+          )
+        })}
       </Pane>
     );
   }
-
- 
 
   openBurgerMenu() {
     const { openMenu } = this.state;
@@ -120,17 +76,10 @@ class HorizontalMenu extends React.Component {
     this.setState({ openMenu: false })
   }
 
-  handleChange(toWhere) {
-    const { history } = this.props;
-
-    if(toWhere) history.push(toWhere);
-    this.closeBurgerMenu();
-  }
-
   render() {
     const { openMenu } = this.state;
-// <Logo src={logo} style={{ height: LOGO_HEIGHT }}/>
-// <Logo src={logoDark} style={{ height: LOGO_HEIGHT, margin: MARGIN }}/>
+    const { logo, listLogo, ...style } = this.props;
+
     return (
       <>
       <Pane
@@ -144,12 +93,13 @@ class HorizontalMenu extends React.Component {
         top="0"
         left="0"
         zIndex={9}
+        {...style}
       >
         <Text
           height="auto"
           margin={MARGIN}
         >
-          
+          {logo || null}
         </Text>
         <Text
           height="auto"
@@ -170,9 +120,9 @@ class HorizontalMenu extends React.Component {
         isShown={openMenu}
         width='100%'
         onCloseComplete={() => this.closeBurgerMenu()}
-      > 
+      >
         <Pane display='flex' justifyContent='space-between' alignItems='center'>
-          
+          {listLogo || null}
           <IconButton
             icon="cross"
             appearance="minimal"
@@ -188,11 +138,16 @@ class HorizontalMenu extends React.Component {
 }
 
 HorizontalMenu.defaultProps = {
-
+  secondaryMenu: [],
+  selectedColor: COLORS.SELECTED
 }
 
 HorizontalMenu.propTypes = {
-
+  primaryMenu: PropTypes.arrayOf(PropTypes.object).isRequired,
+  secondaryMenu: PropTypes.arrayOf(PropTypes.object),
+  selectedColor: PropTypes.string,
+  logo: PropTypes.elementType,
+  listLogo: PropTypes.elementType
 }
 
 export default HorizontalMenu;
